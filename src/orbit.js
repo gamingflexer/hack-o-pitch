d99f43f38bf1a46b0eda0a82a5f2d69ade439885
porridgewithraisins <santhanambr2002@gmail.com>
2022-05-07 21:36:46 +0530

    added logstore

--- src/orbit.js
+++ src/orbit.js
@@ -1,21 +1,27 @@
 const IPFS = require("ipfs-http-client");
 const OrbitDB = require("orbit-db");
 
-const ipfs = IPFS.create({url: 'http://localhost:5001'});
+const ipfs = IPFS.create({ url: "http://localhost:5001" });
 
 class Repository {
     constructor() {
-        this.dbs = {};
+        this.keyValueDbs = {};
+        this.logDbs = {};
         OrbitDB.createInstance(ipfs).then(orbit => (this.orbit = orbit));
     }
 
     async init(projectId) {
-        this.dbs[projectId] = await this.orbit.keyvalue(`${projectId}`);
+        this.keyValueDbs[projectId] = await this.orbit.keyvalue(`${projectId}`);
+        this.logDbs[projectId] = await this.orbit.log(`${projectId}`);
         this.inited = true;
     }
 
-    getDb(projectId) {
-        return this.dbs[projectId];
+    getKeyValueDb(projectId) {
+        return this.keyValueDbs[projectId];
+    }
+
+    getLogDb(projectId) {
+        return this.logDbs[projectId];
     }
 }
 
--- src/routes/orbit.js
+++ src/routes/orbit.js
@@ -1,9 +1,3 @@
-/**
- * create database
- * kv get
- * kv put
- * kv delete
- */
 const { repo } = require("../orbit");
 
 module.exports.createDb = async (req, res, next) => {
@@ -12,26 +6,46 @@ module.exports.createDb = async (req, res, next) => {
     res.send({ msg: "OK" });
 };
 module.exports.kvGet = async (req, res, next) => {
-    const db = repo.getDb(req.params.project_id);
+    const db = repo.getKeyValueDb(req.params.project_id);
     const { key } = req.params;
     res.send({
         val: db.get(key),
     });
 };
 module.exports.kvPut = async (req, res, next) => {
-    const db = repo.getDb(req.params.project_id);
+    const db = repo.getKeyValueDb(req.params.project_id);
     const { key, val } = req.body;
     await db.put(key, val);
     res.send({ msg: "OK" });
 };
 module.exports.kvDel = async (req, res, next) => {
-    const db = repo.getDb(req.params.project_id);
+    const db = repo.getKeyValueDb(req.params.project_id);
     const { key } = req.params;
     const mh = await db.del(key);
     res.send({ msg: "OK", mh });
 };
 
 module.exports.kvAll = async (req, res, next) => {
-    const db = repo.getDb(req.params.project_id);
+    const db = repo.getKeyValueDb(req.params.project_id);
     res.send({ all: db.all });
 };
+
+module.exports.logAdd = async (req, res, next) => {
+    const db = repo.getLogDb(req.params.project_id);
+    const log = req.body;
+    const hash = await db.add(log);
+    res.send({ hash });
+};
+
+module.exports.logGet = async (req, res, next) => {
+    const db = repo.getLogDb(req.params.project_id);
+    const { hash } = req.params;
+    const log = db.get(hash).map(e => e.payload.value);
+    res.send({ log });
+};
+
+module.exports.logGetAll = async (req, res, next) => {
+    const db = repo.getLogDb(req.params.project_id);
+    const all = db.iterator(req.body).collect().map(e => e.payload.value);
+    res.send(all);
+}
