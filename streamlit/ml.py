c287ce029b3f2ebedc12e9ab4801ca5beb9811ab
gamingflexer <osurve20ece@student.mes.ac.in>
2022-05-15 23:44:51 +0530

    Anylatics streamlit | V0.1

--- streamlit/ml.py
+++ streamlit/ml.py
@@ -1,24 +1,46 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO
-
+from bokeh.plotting import figure
+import numpy as np
 
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
+     st.markdown("""---""")
 
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-     st.write(stringio)
+     data = pd.read_csv(uploaded_file)
+     st.write(data)
+     
+     st.markdown("""---""")
 
-     # To read file as string:
-     string_data = stringio.read()
-     st.write(string_data)
+     st.write("Status Request Line Chart")
+     st.line_chart(data['status'])
+     
+     st.markdown("""---""")
 
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+st.markdown("""---""")
+chart_data = pd.DataFrame(
+     np.random.randn(50, 3),
+     columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+st.markdown("""---""")
+df = pd.DataFrame(
+     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+     columns=['lat', 'lon'])
+
+st.map(df)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
